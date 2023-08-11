463716a3b63c5f06d5297c109d23b65bb1b1f0f3
Erfan Golkar <mr.golkaar@gmail.com>
2023-08-11 15:09:26 +0330

    feat: auto slideshow

--- public/js/script.js
+++ public/js/script.js
@@ -29,3 +29,29 @@ function showSlide(n) {
   slides[slideIndex-1].style.display = "block";
   bullets[slideIndex-1].className += " active-bullet";
 }
+
+// auto slideshow
+let autoSlideIndex = 0;
+autoSliding();
+
+function autoSliding() {
+  let i, slides, slidesLen, bullets, bulletsLen;
+  slides = document.getElementsByClassName("slide");
+  slidesLen = slides.length;
+  bullets = document.getElementsByClassName("bullet");
+  bulletsLen = bullets.length;
+
+  autoSlideIndex++;
+  
+  if (autoSlideIndex > slidesLen) {autoSlideIndex = 1}
+  
+  for (i = 0; i < slidesLen; i++) {
+    slides[i].style.display = "none";
+    bullets[i].className = bullets[i].className.replace(" active-bullet", "");
+  }
+  
+  slides[autoSlideIndex-1].style.display = "block";
+  bullets[autoSlideIndex-1].className += " active-bullet";
+
+  setTimeout(autoSliding, 5000);
+}
